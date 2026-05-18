23978c9d00a62b4f5fd5eceeed5e7e2501b25d46
itsdev04 <imkrdev@gmail.com>
2026-05-19 00:31:08 +0530

    basic setup for aws

---
+++ src/main/java/com/devworks/AwsDemoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.devworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwsDemoServiceApplication.class, args);
+  }
+}
