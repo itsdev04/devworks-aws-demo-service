6eccc9f0dc4335b2dbeb713617df59ee259fd838
itsdev04 <imkrdev@gmail.com>
2026-06-24 01:48:14 +0530

    commented sqs,sns,s3 code

--- src/main/java/com/devworks/listener/QueueListener.java
+++ src/main/java/com/devworks/listener/QueueListener.java
@@ -1,15 +1,15 @@
-package com.devworks.listener;
-
-import io.awspring.cloud.sqs.annotation.SqsListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class QueueListener {
-
-  // Spring Boot automatically listens to this queue asynchronously
-  @SqsListener("imkrdev-sqs-queue")
-  public void listenToMessages(String message) {
-    System.out.println("|| SQS MESSAGE RECEIVED ||");
-    System.out.println("Processing file event data: " + message);
-  }
-}
+// package com.devworks.listener;
+//
+// import io.awspring.cloud.sqs.annotation.SqsListener;
+// import org.springframework.stereotype.Component;
+//
+// @Component
+// public class QueueListener {
+//
+//  // Spring Boot automatically listens to this queue asynchronously
+//  @SqsListener("imkrdev-sqs-queue")
+//  public void listenToMessages(String message) {
+//    System.out.println("|| SQS MESSAGE RECEIVED ||");
+//    System.out.println("Processing file event data: " + message);
+//  }
+// }
